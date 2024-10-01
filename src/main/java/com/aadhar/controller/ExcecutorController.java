6aca6303b5152be5216d28a4924d7ced957ea46a
marquisdegramont <142470232+marquisdegramont@users.noreply.github.com>
2024-10-01 17:14:09 +0530

    ad Executor service

---
+++ src/main/java/com/aadhar/controller/ExcecutorController.java
@@ -0,0 +1,21 @@
+package com.aadhar.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/update")
+public class ExcecutorController {
+    @GetMapping("/set")
+    public String getIds(){
+
+
+        return null;
+    }
+
+
+
+
+
+}
